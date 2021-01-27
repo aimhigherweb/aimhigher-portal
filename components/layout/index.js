6a7dac13711813d8774acd2c7892bbb6d637c912
Amy Kapernick <amy@aimhigherweb.design>
2021-01-27 21:08:58 +0800

    added secre page to auth

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,99 +1,84 @@
-import { Fragment, useEffect, useState } from 'react';
+import {
+	Fragment, useEffect, useState, createContext, useContext
+} from 'react';
 import Head from 'next/head';
 
 import Footer from '../partials/footer';
 import Header from '../partials/header';
 import netlifyAuth from '../../lib/netlifyAuth';
+import { UserContext } from '../../pages/_app';
 
 import styles from './layout.module.scss';
 
+// eslint-disable-next-line one-var
 const Layout = ({
-		children, meta, site
-	}) => {
-		const [loggedIn, setLoggedIn] = useState(netlifyAuth.isAuthenticated),
-			[currentUser, setUser] = useState(null),
-			login = () => {
-				netlifyAuth.authenticate((user) => {
-					setLoggedIn(!!user);
-					setUser(user);
-					netlifyAuth.closeModal();
-				});
-			},
-			logout = () => {
-				netlifyAuth.signout(() => {
-					setLoggedIn(false);
-					setUser(null);
-				});
-			};
-
-		useEffect(() => {
-			netlifyAuth.initialize((user) => {
-				setLoggedIn(!!user);
-				setUser(user);
-			});
-		}, [loggedIn]);
-
-		return (
-			<Fragment>
-				{/* <Meta {...{ ...meta, ...site }} /> */}
-				<Header />
-				<button onClick={login}>
-					{loggedIn
-						? <span>Logged in as {currentUser?.user_metadata.full_name}</span>
-						: <span>Log in</span>}
-				</button>
-				{loggedIn && <button onClick={logout}>
+	children, meta, site
+}) => {
+	const {
+		user, loggedIn, login, logout
+	} = useContext(UserContext);
+	return (
+		<Fragment>
+			{/* <Meta {...{ ...meta, ...site }} /> */}
+			<Header />
+			<button onClick={login}>
+				{loggedIn
+					? <span>Logged in as {user?.user_metadata.full_name}</span>
+					: <span>Log in</span>}
+			</button>
+			{loggedIn && <button onClick={logout}>
 					Log out
-				</button>}
-				<main>
-					{children}
-				</main>
-				<Footer />
-			</Fragment>
-		);
-	},
+			</button>}
+			<main>
+				{children}
+			</main>
+			<Footer />
+		</Fragment>
+	);
+};
 
-	Meta = ({
-		metaDesc, metaTitle, title, description, url, favicon, slug, opengraphImage
-	}) => {
-		const path = slug || ``,
-			page_title = metaTitle || title,
-			page_desc = metaDesc || description,
-			siteUrl = url;
+// eslint-disable-next-line one-var
+const Meta = ({
+	metaDesc, metaTitle, title, description, url, favicon, slug, opengraphImage
+}) => {
+	const path = slug || ``,
+		page_title = metaTitle || title,
+		page_desc = metaDesc || description,
+		siteUrl = url;
 
-		return (
-			<Head>
-				<meta charSet="utf-8" />
-				<meta httpEquiv="x-ua-compatible" content="ie=edge" />
-				<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no, maximum-scale=1" />
-				<title>{page_title}</title>
-				<meta name="description" content={page_desc} />
-				<meta name="author" content={page_title} />
-				<link rel="canonical" href={`${siteUrl}${path}`} />
-				<base href="/" />
+	return (
+		<Head>
+			<meta charSet="utf-8" />
+			<meta httpEquiv="x-ua-compatible" content="ie=edge" />
+			<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no, maximum-scale=1" />
+			<title>{page_title}</title>
+			<meta name="description" content={page_desc} />
+			<meta name="author" content={page_title} />
+			<link rel="canonical" href={`${siteUrl}${path}`} />
+			<base href="/" />
 
-				<link rel="shortcut icon" href={favicon.sourceUrl} />
-				<link rel="icon" sizes="192x192" href={favicon.sourceUrl} />
-				<link rel="apple-touch-icon" href={favicon.sourceUrl} />
-				<meta name="theme-color" content="#007cbb" />
-				<link rel="mask-icon" href={favicon.sourceUrl} color="#007cbb" />
+			<link rel="shortcut icon" href={favicon.sourceUrl} />
+			<link rel="icon" sizes="192x192" href={favicon.sourceUrl} />
+			<link rel="apple-touch-icon" href={favicon.sourceUrl} />
+			<meta name="theme-color" content="#007cbb" />
+			<link rel="mask-icon" href={favicon.sourceUrl} color="#007cbb" />
 
-				{/* Facebook */}
-				<meta property="og:url" content={`${siteUrl}${path}`} />
-				<meta property="og:title" content={page_title} />
-				<meta property="og:description" content={page_desc} />
-				<meta property="og:type" content="website" />
-				{opengraphImage && (
-					<meta
-						property="og:image"
-						content={opengraphImage.sourceUrl}
-					/>
-				)}
+			{/* Facebook */}
+			<meta property="og:url" content={`${siteUrl}${path}`} />
+			<meta property="og:title" content={page_title} />
+			<meta property="og:description" content={page_desc} />
+			<meta property="og:type" content="website" />
+			{opengraphImage && (
+				<meta
+					property="og:image"
+					content={opengraphImage.sourceUrl}
+				/>
+			)}
 
-				{/* Twitter */}
-				<meta name="twitter:card" content="summary_large_image" />
-			</Head>
-		);
-	};
+			{/* Twitter */}
+			<meta name="twitter:card" content="summary_large_image" />
+		</Head>
+	);
+};
 
 export default Layout;
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,42 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, createContext } from 'react';
 import netlifyAuth from '../lib/netlifyAuth';
 
 // import '../styles/global.scss';
+export const UserContext = createContext(null);
 
-function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
-}
+const App = ({ Component, pageProps }) => {
+	const [loggedIn, setLoggedIn] = useState(netlifyAuth.isAuthenticated),
+		[currentUser, setUser] = useState(null),
+		login = () => {
+			netlifyAuth.authenticate((user) => {
+				setLoggedIn(!!user);
+				setUser(user);
+				netlifyAuth.closeModal();
+			});
+		},
+		logout = () => {
+			netlifyAuth.signout(() => {
+				setLoggedIn(false);
+				setUser(null);
+			});
+		};
 
-export default MyApp;
+	useEffect(() => {
+		netlifyAuth.initialize((user) => {
+			setLoggedIn(!!user);
+			setUser(user);
+		});
+	}, [loggedIn]);
+
+	// console.log(currentUser);
+
+	return (
+		<UserContext.Provider value={{
+			user: currentUser, loggedIn, login, logout
+		}}>
+			<Component {...pageProps} />
+		</UserContext.Provider>
+	);
+};
+
+export default App;
