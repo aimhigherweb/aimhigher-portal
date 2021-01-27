1fd6d5ee67028ec2176dbc90e868897ac09bde87
Amy Kapernick <amy@aimhigherweb.design>
2021-01-27 18:32:05 +0800

    changed dynamic route

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,5 +1,11 @@
-const DocPage = ({ params }) => (
-	<h1>Doc Page {params.slug}</h1>
-);
+import { useRouter } from 'next/router';
+
+const DocPage = ({ params }) => {
+	const router = useRouter(),
+		{ slug } = router.query;
+	return (
+		<h1>Doc Page {slug}</h1>
+	);
+};
 
 export default DocPage;
