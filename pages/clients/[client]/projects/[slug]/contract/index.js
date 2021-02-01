3e2e43278685647aa91163147daec0cb24e152ff
Amy Kapernick <amy@aimhigherweb.design>
2021-02-01 21:20:57 +0800

    fixed templates

--- pages/clients/[client]/projects/[slug]/contract/index.js
+++ pages/clients/[client]/projects/[slug]/contract/index.js
@@ -3,22 +3,20 @@ import Layout from '../../../../../../components/layout';
 import RestrictedPage from '../../../../../../components/parts/restricted_page';
 // TODO: Replace with actual data from CMS
 import projectData from '../../../../../../_data/templates/client/project';
-import clientData from '../../../../../../_data/templates/client';
-import contractData from '../../../../../../_data/templates/client/project/contract';
-
-import processMarkdown from '../../../../../../utils/markdown';
+// import clientData from '../../../../../../_data/templates/client';
+// import contractData from '../../../../../../_data/templates/client/project/contract';
 
 const ProjectContract = () => {
 	const router = useRouter(),
 		{ client } = router.query,
-		access_roles = [projectData.client],
-		contract = processMarkdown(contractData.template);
+		access_roles = [projectData.client];
+		// contract = processMarkdown(contractData.template);
 
 	return (
 		<Layout>
 			<RestrictedPage {...{ access_roles }}>
 				<h1>{projectData.name} - Contract</h1>
-				<div dangerouslySetInnerHTML={{ __html: contract }} />
+				{/* <div dangerouslySetInnerHTML={{ __html: contract }} /> */}
 			</RestrictedPage>
 		</Layout>
 	);
