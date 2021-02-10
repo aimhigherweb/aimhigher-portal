1423c35cbd21bdd02609a8ec5bb8fd87d58a2816
Amy Kapernick <amy@aimhigherweb.design>
2021-02-10 10:00:21 +0800

    changed access function to be clearer what I'm checking

--- utils/auth/checkAccess.js
+++ utils/auth/checkAccess.js
@@ -1,8 +1,8 @@
 const checkAccess = (user_roles, access_roles) => {
-	return access_roles.some((role) => user_roles?.includes(role)) 
-	|| user_roles?.includes(`admin`) 
-	|| access_roles.includes('public')
-	|| access_roles == []
+	const hasRole = access_roles.some((role) => user_roles?.includes(role))
+	const isAdmin = user_roles?.includes(`admin`) 
+	const isPublic = access_roles.includes('public') || access_roles == []
+	return  hasRole || isAdmin || isPublic
 }
 
 export default checkAccess
